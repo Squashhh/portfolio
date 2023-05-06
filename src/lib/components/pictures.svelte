a1d1d4c45ca4abdcb1e0c5108fe2b29f0ddb8d5b
Vincent V <vincent.veron.contact@gmail.com>
2023-05-06 16:29:21 +0200

    💢REMOVE ATTRIBUTION

--- src/lib/components/pictures.svelte
+++ src/lib/components/pictures.svelte
@@ -5,19 +5,16 @@
     id: 0,
     name: "CAULDRON",
     imgurl: "/images/carousel/one.png",
-    attribution: "cosmic-timetraveler-pYyOZ8q7AII-unsplash.com",
   },
   {
     id: 1,
     name: "LARGUAGE",
     imgurl: "/images/carousel/two.png",
-    attribution: "cristina-gottardi-CSpjU6hYo_0-unsplash.com",
   },
   {
     id: 2,
     name: "CANNON",
     imgurl: "/images/carousel/three.png",
-    attribution: "johannes-plenio-RwHv7LgeC7s-unsplash.com",
   }
 ];
 let showThumbs=false
