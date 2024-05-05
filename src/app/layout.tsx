2f78c0707f52c42e4783ddb485dba08afed8f1e8
Vincent V. <vincent.veron.contact@gmail.com>
2024-05-05 16:10:18 +0200

    Web Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <SpeedInsights/>
+      <Analytics/>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
